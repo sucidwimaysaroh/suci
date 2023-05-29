1eaa04ec40191c623a8209d6506470b331c191da
unknown <sucidm12@gmail.com>
2023-05-29 13:16:44 +0700

    api dan optimasi tampilan website

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,43 +1,71 @@
+import $ from 'jquery';
+
 const SerpApi = require('google-search-results-nodejs');
 const search = new SerpApi.GoogleSearch("de1b595221101a2f56c123b50f1d4f3ff63fb968f1d9052f2fa7286a511a02a9");
 
-const params = {
-    engine: "home_depot",
-    q: "furniture",
-    hd_sort: "top_sellers"
-  };
-  
-const callback = function(data) {
-    for (let i = 0 ; i < recommPic.length ; i++) {
-        recommPic[i].src = `${data["products"][i].thumbnails[0][5]}`;
+function params (query) {
+    return {
+        engine: "home_depot",
+        q: query,
+        hd_sort: "best_match" 
     }
-    for (let i = 0 ; i < bestseller.length ; i++) {
-        bestseller[i].src = `${data["products"][i].thumbnails[0][5]}`;
-    }
-    let urls = `${data["products"][10].thumbnails[0][5]}`
-    document.getElementsByClassName('hero-desc')[0].style.backgroundImage = urls;
-};
-
-search.json(params, callback);
-
-let recommPic = document.getElementsByClassName('recomm-picture');
-let bestseller = document.getElementsByClassName('header-photos')
-
-let acc = document.getElementsByClassName("accordion");
-for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    let panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-      this.style.backgroundColor = '#FCFAFA';
-      this.parentElement.style.backgroundColor = '#FCFAFA';
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-      panel.style.backgroundColor = '#E5F0B6';
-      this.style.backgroundColor = '#E5F0B6';
-      this.parentElement.style.backgroundColor = '#E5F0B6';
-    } 
-  });
 }
 
+function callbackCategory (data) {
+    $('.recomm-picture').each(function(index, value) {
+        $(value).attr('src', `${data["products"][index].thumbnails[0][5]}`);
+    })
+}
+
+function callbackBestSeller (data) {
+    $('.hero-desc').css('background-image', `url(${data["products"][1].thumbnails[0][5]})`);
+    $('.hero-desc-pricebtn').find('span').text(`$${Math.round(data["products"][1].price_was)}`);
+    $('.hero-desc-pricebtn').find('p').text(data["products"][1].brand);
+    $('.header-photos').each(function(index, value) {
+        $(value).attr('src', `${data["products"][index].thumbnails[0][5]}`);
+    })
+}
+
+search.json(params('Best Home Furniture'), callbackBestSeller)
+
+$('.accordion').each(function(index, value) {
+    let $thisAcc = value;
+    $($thisAcc).on('click', function () {
+        console.log($($thisAcc).text())
+        search.json(params(`Best ${$($thisAcc).text()} set`), callbackCategory)
+        $('.accordion').each(function(index, value) {
+            if ($thisAcc == value) {
+                return;
+            } else {
+                $(value).next().css('max-height', '0px');
+                $(value).css('background-color', '#FCFAFA');
+                $(value).next().css('background-color', '#FCFAFA');
+                $(value).parent().css('background-color', '#FCFAFA');
+            }
+        })
+        if ($($thisAcc).next().css('max-height') != '0px') {
+            $($thisAcc).next().css('max-height', '0px');
+            $($thisAcc).css('background-color', '#FCFAFA');
+            $($thisAcc).next().css('background-color', '#FCFAFA');
+            $($thisAcc).parent().css('background-color', '#FCFAFA');
+        } else {
+            $($thisAcc).next().css('max-height', `${$($thisAcc).next()[0].scrollHeight}px`);
+            $($thisAcc).css('background-color', '#E5F0B6');
+            $($thisAcc).next().css('background-color', '#E5F0B6');
+            $($thisAcc).parent().css('background-color', '#E5F0B6');
+        }
+    })
+})
+
+$('.recomm-picture').each(function (index, value) {
+    let $thisPic = value;
+    $($thisPic).on('click', function () {
+        $('.recomm-picture').each(function (index, value) {
+            $(value).removeClass('recomm-picture-selected');
+            console.log(value)
+        })
+        $($thisPic).addClass('recomm-picture-selected');
+        console.log($thisPic)
+    })
+    console.log('ss')
+})
